e7a4c6f135d0956ca280fd972f675584312766f5
[jack mkimbo] <[nickihllj@gmail.com]>
2022-02-27 01:53:10 +0300

    added frontend url in allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,6 +2,7 @@ const allowedOrigins = [
   "https://www.yoursite.com",
   "http://127.0.0.1:5500",
   "http://localhost:3000",
+  "https://mern-mini-social.netlify.app",
 ];
 
 module.exports = allowedOrigins;
