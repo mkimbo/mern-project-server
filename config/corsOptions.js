a9e736fa4d1892bdcf88fa9f25aededb16101d43
[jack mkimbo] <[nickihllj@gmail.com]>
2022-02-27 17:54:52 +0300

    added netlify redirects

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,6 +1,6 @@
-const allowedOrigins = require('./allowedOrigins');
+const allowedOrigins = require("./allowedOrigins");
 
-const corsOptions = {
+/* const corsOptions = {
     origin: (origin, callback) => {
         if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
             callback(null, true)
@@ -9,6 +9,12 @@ const corsOptions = {
         }
     },
     optionsSuccessStatus: 200
-}
+} */
+
+const corsOptions = {
+  origin: allowedOrigins,
+  credentials: true,
+  methods: ["GET", "POST"],
+};
 
 module.exports = corsOptions;
--- config/credentials.js
+++ config/credentials.js
@@ -3,8 +3,8 @@ const allowedOrigins = require("./allowedOrigins");
 const credentials = (req, res, next) => {
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Credentials", true);
-    res.header("Access-Control-Allow-Origin", origin);
   }
   next();
 };
